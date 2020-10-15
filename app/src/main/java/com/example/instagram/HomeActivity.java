43132a13cc270dc17fd357f9347deea764a73c06
eslamlii <preslamali@hotmail.com>
2020-10-15 03:00:23 +0200

    update

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -1,16 +1,14 @@
 package com.example.instagram;
 
-import android.os.Build;
-import android.os.Bundle;
-import android.view.WindowManager;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -0,0 +1,107 @@
+package com.example.instagram;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private Context mContext;
+    private EditText mEmail;
+    private EditText mPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mContext = LoginActivity.this;
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+
+        setupFirebaseAuth();
+        init();
+    }
+
+    private boolean isStringNull(String string) {
+        return string.equals("");
+    }
+
+    private void init() {
+        Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+
+                if (isStringNull(email) && isStringNull(password)) {
+                    Toast.makeText(mContext, "You must fill out all the fields", Toast.LENGTH_LONG).show();
+                } else {
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        startActivity(new Intent(mContext, HomeActivity.class));
+                                        finish();
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Failed", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+    }
+
+    private void setupFirebaseAuth() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    Toast.makeText(mContext, "Signed in ", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(mContext, HomeActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(mContext, "Not signed in", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
