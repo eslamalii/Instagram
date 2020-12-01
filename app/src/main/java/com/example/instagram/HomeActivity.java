4f5e52b9b53f57a99cc6372d83f3fa5bb4029b47
eslamlii <preslamali@hotmail.com>
2020-12-01 03:04:45 +0200

    update

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -1,14 +1,31 @@
 package com.example.instagram;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+//        bottomNavigationView = findViewById(R.id.bottomNav);
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
     }
+
+//    private BottomNavigationView.OnNavigationItemReselectedListener bottomNavMethod =
+//            new BottomNavigationView.OnNavigationItemReselectedListener() {
+//        @Override
+//        public void onNavigationItemReselected(@NonNull MenuItem item) {
+//
+//        }
+//    };
 }
