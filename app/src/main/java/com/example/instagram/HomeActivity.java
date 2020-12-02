7b7f61f9a69874ac948b3b5c577f63e033ebf79d
eslamlii <preslamali@hotmail.com>
2020-12-02 03:43:10 +0200

    update

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -5,6 +5,7 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,16 +17,33 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-//        bottomNavigationView = findViewById(R.id.bottomNav);
-//
-//        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
     }
 
-//    private BottomNavigationView.OnNavigationItemReselectedListener bottomNavMethod =
-//            new BottomNavigationView.OnNavigationItemReselectedListener() {
-//        @Override
-//        public void onNavigationItemReselected(@NonNull MenuItem item) {
-//
-//        }
-//    };
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment fragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.home:
+                            fragment = new HomeFragment();
+                            break;
+                        case R.id.activity:
+                            fragment = new ActivityFragment();
+                            break;
+                        case R.id.profile:
+                            fragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+                    return true;
+                }
+            };
 }
