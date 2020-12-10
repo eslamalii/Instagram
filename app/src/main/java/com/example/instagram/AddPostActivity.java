af886dbf3d6d28da81dd9e1fd13a94a1ed37cba3
eslamlii <preslamali@hotmail.com>
2020-12-10 05:33:20 +0200

    update

--- app/src/main/java/com/example/instagram/AddPostActivity.java
+++ app/src/main/java/com/example/instagram/AddPostActivity.java
@@ -1,6 +1,8 @@
 package com.example.instagram;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -27,11 +30,15 @@ public class AddPostActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser user;
     private ProgressDialog mProgressDialog;
+    private static final int GALLARY_CODE = 1;
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+
+        mProgressDialog = new ProgressDialog(this);
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
@@ -42,6 +49,15 @@ public class AddPostActivity extends AppCompatActivity {
         mPostDes = findViewById(R.id.desPost);
         mPostImage = findViewById(R.id.addImagePost);
         mSubmit = findViewById(R.id.submitPost);
+
+        mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLARY_CODE);
+            }
+        });
 
         mSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,10 +71,19 @@ public class AddPostActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLARY_CODE && resultCode == RESULT_OK){
+            mImageUri = data.getData();
+            mPostImage.setImageURI(mImageUri);
+        }
+    }
+
     private void startPosting() {
 
-//        mProgressDialog.setMessage("Posting to blog.....");
-//        mProgressDialog.show();
+        mProgressDialog.setMessage("Posting to blog.....");
+        mProgressDialog.show();
 
         String titleVel = mPostTitle.getText().toString().trim();
         String desVel = mPostDes.getText().toString().trim();
@@ -71,7 +96,7 @@ public class AddPostActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(Void aVoid) {
                     Toast.makeText(AddPostActivity.this, "Added", Toast.LENGTH_SHORT).show();
-//                    mProgressDialog.dismiss();
+                    mProgressDialog.dismiss();
                 }
             });
         }
