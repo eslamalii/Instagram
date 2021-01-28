1f42d4ef7a51eb64bca8b4ce22fb6b06822b874d
eslamlii <preslamali@hotmail.com>
2021-01-28 04:24:29 +0200

    update

--- app/src/main/java/com/example/instagram/Activities/Create_Acc.java
+++ app/src/main/java/com/example/instagram/Activities/Create_Acc.java
@@ -19,6 +19,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -31,9 +34,11 @@ public class Create_Acc extends AppCompatActivity {
     private Button createAccount;
     private DatabaseReference mDatabaseReference;
     private FirebaseAuth mFirebaseAuth;
+    private StorageReference mFirebaseStorage;
     private FirebaseDatabase mFirebaseDatabase;
     private ProgressDialog progressDialog;
     private ImageButton profileImage;
+    private Uri resultUri = null;
     private final static int GALLERY_CODE = 1;
 
     @Override
@@ -45,6 +50,8 @@ public class Create_Acc extends AppCompatActivity {
         mDatabaseReference = mFirebaseDatabase.getReference().child("MUsers");
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mFirebaseStorage = FirebaseStorage.getInstance().getReference().child("MBlog_profile_Pics");
 
         progressDialog = new ProgressDialog(this);
 
@@ -87,18 +94,27 @@ public class Create_Acc extends AppCompatActivity {
                 @Override
                 public void onSuccess(AuthResult authResult) {
                     if (authResult != null) {
-                        String userId = mFirebaseAuth.getCurrentUser().getUid();
-                        DatabaseReference currentUserDb = mDatabaseReference.child(userId);
 
-                        currentUserDb.child("firstname").setValue(name);
-                        currentUserDb.child("lastname").setValue(lname);
-                        currentUserDb.child("image").setValue("none");
+                        StorageReference imagePath = mFirebaseStorage.child("MBlog_profile_Pics")
+                                .child(resultUri.getLastPathSegment());
 
-                        progressDialog.dismiss();
+                        imagePath.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                String userId = mFirebaseAuth.getCurrentUser().getUid();
+                                DatabaseReference currentUserDb = mDatabaseReference.child(userId);
 
-                        Intent intent = new Intent(Create_Acc.this, HomeActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
+                                currentUserDb.child("firstname").setValue(name);
+                                currentUserDb.child("lastname").setValue(lname);
+                                currentUserDb.child("image").setValue(resultUri.toString());
+
+                                progressDialog.dismiss();
+
+                                Intent intent = new Intent(Create_Acc.this, HomeActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                            }
+                        });
                     }
                 }
             });
@@ -108,12 +124,23 @@ public class Create_Acc extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK){
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
             Uri mImageUri = data.getData();
 
             CropImage.activity(mImageUri)
+                    .setAspectRatio(1, 1)
                     .setGuidelines(CropImageView.Guidelines.ON)
                     .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                resultUri = result.getUri();
+                profileImage.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
         }
     }
 }
